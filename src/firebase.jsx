ccfb91286b63cf1f56d83dc35e7f39442e0d382a
MetaAffinity <metaaffinity@gmail.com>
2023-09-29 02:12:46 +0500

    chatapp

---
+++ src/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJ59v-Sfh-tXRDACx64Z2gY9reApj3Nqw",
+  authDomain: "metaaffinitychatapp.firebaseapp.com",
+  projectId: "metaaffinitychatapp",
+  storageBucket: "metaaffinitychatapp.appspot.com",
+  messagingSenderId: "183433633774",
+  appId: "1:183433633774:web:56e0eb6957a4092467f8ff"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
